25da9f84bf742c4ccfa84d22fec64bc07820b9f9
Lucas Chaves <gabrielucas2008@hotmail.com>
2023-11-23 23:15:26 -0300

    implementação e testes de unidade para calculadora simples em python

---
+++ calculadora.py
@@ -0,0 +1,38 @@
+class Calculadora:
+
+  def soma(self, a, b):
+    return a + b
+
+  def subtracao(self, a, b):
+    return a - b
+
+  def multiplicacao(self, a, b):
+    return a * b
+
+  def divisao(self, a, b):
+    return a / b
+
+  def calcula(self):
+    print("Calculadora simples")
+    print("Selecione a operação desejada:")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+
+    operacao = input("Operação: ")
+    numero1 = float(input("Número 1: "))
+    numero2 = float(input("Número 2: "))
+
+    if operacao == "1":
+      resultado = soma(numero1, numero2)
+    elif operacao == "2":
+      resultado = subtracao(numero1, numero2)
+    elif operacao == "3":
+      resultado = multiplicacao(numero1, numero2)
+    elif operacao == "4":
+      resultado = divisao(numero1, numero2)
+    else:
+      print("Operação inválida.")
+
+    print("Resultado:", resultado)
---
+++ testes_calculadora.py
@@ -0,0 +1,28 @@
+import unittest
+
+from calculadora import Calculadora
+
+
+class CalculadoraTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calculadora = Calculadora()
+
+    def test_soma(self):
+        resultado = self.calculadora.soma(1, 2)
+        self.assertEqual(resultado, 3)
+
+    def test_subtracao(self):
+        resultado = self.calculadora.subtracao(1, 2)
+        self.assertEqual(resultado, -1)
+
+    def test_multiplicacao(self):
+        resultado = self.calculadora.multiplicacao(1, 2)
+        self.assertEqual(resultado, 2)
+
+    def test_divisao(self):
+        resultado = self.calculadora.divisao(1, 2)
+        self.assertEqual(resultado, 0.5)
+
+if __name__ == "__main__":
+    unittest.main()
